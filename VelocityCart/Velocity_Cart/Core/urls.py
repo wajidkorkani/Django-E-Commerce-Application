41284e17f610544a3fc18048c9c7f03a054e9f45
wajidkorkani <errormrcoder@gmail.com>
2023-11-21 17:14:25 +0500

    Product about page is created. Now after clicking on product from home page user will be easily redirect to the about page of that product.

--- VelocityCart/Velocity_Cart/Core/urls.py
+++ VelocityCart/Velocity_Cart/Core/urls.py
@@ -3,4 +3,5 @@ from Core.views import *
 
 urlpatterns = [
     path('', Home, name='login'),
+    path('about-product/<int:product_id>/', Product_About_Page, name='Product_About_Page'),
 ]
--- VelocityCart/Velocity_Cart/Core/views.py
+++ VelocityCart/Velocity_Cart/Core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, Categorey
 # Create your views here.
 
@@ -7,5 +7,13 @@ def Home(request):
     template = 'Core/home.html'
     context = {
         'products' : products,
+    }
+    return render(request, template, context)
+
+def Product_About_Page(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    template = 'Core/Product_About_Page.html'
+    context = {
+        'product' : product,
     }
     return render(request, template, context)
