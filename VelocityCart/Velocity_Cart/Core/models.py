b5861ef33b85a1539eb1fa986719a1f88524d668
wajidkorkani <errormrcoder@gmail.com>
2023-11-21 16:27:34 +0500

    Model for products and products categoery are deffined and registered in admin panel and all products will be are listed on home page.

--- VelocityCart/Velocity_Cart/Core/models.py
+++ VelocityCart/Velocity_Cart/Core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Categorey(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    image1 = models.ImageField(upload_to='media/', blank=True)
+    image2 = models.ImageField(upload_to='media/', blank=True)
+    image3 = models.ImageField(upload_to='media/', blank=True)
+    image4 = models.ImageField(upload_to='media/', blank=True)
+    image5 = models.ImageField(upload_to='media/', blank=True)
+    image6 = models.ImageField(upload_to='media/', blank=True)
+    image7 = models.ImageField(upload_to='media/', blank=True)
+    image8 = models.ImageField(upload_to='media/', blank=True)
+    title = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    categorey = models.ForeignKey(Categorey, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    time_stamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- VelocityCart/Velocity_Cart/Core/views.py
+++ VelocityCart/Velocity_Cart/Core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import Product, Categorey
 # Create your views here.
 
 def Home(request):
+    products = Product.objects.all()
     template = 'Core/home.html'
     context = {
-        'text' : 'Hello django!',
+        'products' : products,
     }
     return render(request, template, context)
