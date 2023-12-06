dbb4ce922aa429fc0a8ba224dfe1d4669f05f97c
wajidkorkani <errormrcoder@gmail.com>
2023-12-06 22:40:45 +0500

    Virtual enviroment is deleted.

--- Velocity_Cart/Velocity_Cart/settings.py
+++ Velocity_Cart/Velocity_Cart/settings.py
@@ -112,14 +112,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
 # Emal settings
